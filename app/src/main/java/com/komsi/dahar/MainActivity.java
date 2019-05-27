af474807c723773bf95ebf5f1421ca69e3e0ded4
Wafi <septya.wafi@gmail.com>
2019-05-27 10:48:10 +0700

    login register

---
+++ app/src/main/java/com/komsi/dahar/MainActivity.java
@@ -0,0 +1,20 @@
+package com.komsi.dahar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+    }
+}
